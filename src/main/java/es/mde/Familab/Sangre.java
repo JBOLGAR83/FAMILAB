9f27bfc7ae9121f36216edcda52421a43b583c84
ROrtiz <juancarlos_bolivargarcia@hotmail.com>
2022-05-05 00:25:07 +0200

    Añadiendo cambios en codigo

--- src/main/java/es/mde/Familab/Sangre.java
+++ src/main/java/es/mde/Familab/Sangre.java
@@ -1,6 +1,39 @@
 package es.mde.Familab;
 
-public interface Sangre {
-	
-	String addSalaExtraccionSangre(Sala_Extraccion sala_Extraccion);
+public class SangreImpl extends Analitica implements Sangre {
+
+	private Sala_Extraccion sala_Extraccion;
+	private Float colesterol;
+	private Float urea;
+
+	public Sala_Extraccion getSala_Extraccion() {
+		return sala_Extraccion;
+	}
+
+	public void setSala_Extraccion(Sala_Extraccion sala_Extraccion) {
+		this.sala_Extraccion = sala_Extraccion;
+	}
+
+	public Float getColesterol() {
+		return colesterol;
+	}
+
+	public void setColesterol(Float colesterol) {
+		this.colesterol = colesterol;
+	}
+
+	public Float getUrea() {
+		return urea;
+	}
+
+	public void setUrea(Float urea) {
+		this.urea = urea;
+	}
+
+	@Override
+	public String addSalaExtraccionSangre(Sala_Extraccion sala_Extraccion) {
+		setSala_Extraccion(sala_Extraccion);
+		return "Lugar de la extraccion: " + sala_Extraccion;
+	}
+
 }
